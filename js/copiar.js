4040517c2cac46d13503c25cbd648adf762981f6
MacaroCunha <xmariacaroline@outlook.com>
2024-02-02 14:16:04 -0300

    feat: integração do js com o html

--- js/copiar.js
+++ js/copiar.js
@@ -1,10 +1,14 @@
-var btnCopiar = document.querySelector(".btn-copiar");
+document.addEventListener('DOMContentLoaded', function () {
+  var btnCopiar = document.querySelector(".btn-copiar");
 
-btnCopiar.onclick = function() {
+  btnCopiar.onclick = function () {
+    var resultado = document.getElementById("resultado");
+
     resultado.select();
     document.execCommand("copy");
     inputTexto.value = "";
     inputTexto.focus();
     resultado.textContent = "";
     btnCopiar.classList.add("invisivel");
-}
+  }
+});
